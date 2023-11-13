54eb73c7092b12a87b8c612d530c00f0cb78862d
guillemulass <gmulbat1301@g.educaand.es>
2023-11-13 17:05:42 +0100

    Initial commit

---
+++ contraseña.13_11_2023/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
+
+namespace contraseña._13_11_2023
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            
+            
+            List<String> lineas = new List<string>(File.ReadAllLines("./2151220-passwords.txt"));
+            
+            Random random = new Random();
+            
+            int numeroAleatorio = random.Next(1,2151219);
+
+            
+            string password = lineas[numeroAleatorio];
+            string encriptedPassword = CalcularSha256(password);
+            
+            Console.WriteLine("Indice : " + numeroAleatorio); 
+            Console.WriteLine("---------------");
+           
+           Stopwatch stopwatch = new Stopwatch();
+           stopwatch.Start();
+           
+           foreach (var i in lineas)
+           {
+               if (encriptedPassword == CalcularSha256(i))
+               {
+                   Console.WriteLine("Contraseña Encriptada : " + encriptedPassword);
+                   Console.WriteLine("Contraseña : " + i);
+                   stopwatch.Stop();
+                   Console.WriteLine("Tiempo transcurrido: " + stopwatch.ElapsedMilliseconds + " ms");
+               }
+           }
+           
+           
+           static string CalcularSha256(string input)
+           {
+               using (SHA256 sha256 = SHA256.Create())
+               {
+                   // Convertir cadena en bytes
+                   byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                   // Calcular el hash
+                   byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                   // Convertir los bytes del hash a una cadena hexadecimal
+                   StringBuilder stringBuilder = new StringBuilder();
+                   foreach (byte b in hashBytes)
+                   {
+                       stringBuilder.Append(b.ToString("x2"));
+                   }
+
+                   return stringBuilder.ToString();
+               }
+           }
+           
+        } 
+    }
+}
+
---
+++ contraseña.13_11_2023/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("contraseña._13_11_2023")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("contraseña._13_11_2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ABA6BAB8-620A-4E49-8BC3-4CE149BD5458")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
