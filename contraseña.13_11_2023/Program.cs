653770a9be59fd9ea862f0d3f7d33ab6ccaea78d
guillemulass <gmulbat1301@g.educaand.es>
2023-11-20 17:36:25 +0100

    casi todo terminado

--- contraseña.13_11_2023/Program.cs
+++ contraseña.13_11_2023/Program.cs
@@ -1,68 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Diagnostics;
+using System.Threading;
 
-namespace contraseña._13_11_2023
+namespace contraseña._13_11_2023;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        public static void Main()
+        string path = "./2151220-passwords.txt";
+        List<string> lines = new List<string>(File.ReadAllLines(path));
+        int totalLineNum = lines.Count;
+
+        Random random = new Random();
+        int randomNumber = random.Next(1, totalLineNum);
+
+        string password = lines[randomNumber];
+        string encryptedPassword = CalculateSha256(password);
+
+        Console.WriteLine($"\nIndice: {randomNumber}\nContraseña: {password}\nContraseña Encriptada: {encryptedPassword}");
+        Console.WriteLine("\n---------------\n");
+
+        int lineDivision = NumberLineDivision(totalLineNum, 8);
+
+        for (int i = 0; i < 8; i++)
         {
-            
-            
-            List<String> lineas = new List<string>(File.ReadAllLines("./2151220-passwords.txt"));
-            
-            Random random = new Random();
-            
-            int numeroAleatorio = random.Next(1,2151219);
+            int startLine = LineDivisor(lineDivision, i);
+            Thread thread = new Thread(() => UnEncrypter(startLine, lines, encryptedPassword, lineDivision));
+            thread.Start();
+        }
+    }
 
-            
-            string password = lineas[numeroAleatorio];
-            string encriptedPassword = CalcularSha256(password);
-            
-            Console.WriteLine("Indice : " + numeroAleatorio); 
-            Console.WriteLine("---------------");
-           
-           Stopwatch stopwatch = new Stopwatch();
-           stopwatch.Start();
-           
-           foreach (var i in lineas)
-           {
-               if (encriptedPassword == CalcularSha256(i))
-               {
-                   Console.WriteLine("Contraseña Encriptada : " + encriptedPassword);
-                   Console.WriteLine("Contraseña : " + i);
-                   stopwatch.Stop();
-                   Console.WriteLine("Tiempo transcurrido: " + stopwatch.ElapsedMilliseconds + " ms");
-               }
-           }
-           
-           
-           static string CalcularSha256(string input)
-           {
-               using (SHA256 sha256 = SHA256.Create())
-               {
-                   // Convertir cadena en bytes
-                   byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+// Encuentra la contraseña encriptada, transformando una lista de contraseñas a hash256 y comprarando cada contraseña
+// con la contreña encriptada
+// @param limit Donde empieza a leer el archivo de contraseñas
+// @param lines Archivo de contraseñas, metido en una lista
+// @param encriptedPassword Contraseña a desencriptar
+// @param numberLineDivision numero de lineas que debe leer
+    static void UnEncrypter(int limit, List<String> lines, string encriptedPassword, int numberLineDivision)
+    {
+        try
+        {
+            var actualLines = lines.GetRange(limit, numberLineDivision);
 
-                   // Calcular el hash
-                   byte[] hashBytes = sha256.ComputeHash(inputBytes);
+            bool continues = true;
 
-                   // Convertir los bytes del hash a una cadena hexadecimal
-                   StringBuilder stringBuilder = new StringBuilder();
-                   foreach (byte b in hashBytes)
-                   {
-                       stringBuilder.Append(b.ToString("x2"));
-                   }
+            while (continues)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                foreach (var i in actualLines)
+                {
+                    if (encriptedPassword == CalculateSha256(i))
+                    {
+                        Console.WriteLine("Contraseña Encriptada : " + encriptedPassword);
+                        Console.WriteLine("Contraseña : " + i);
+                        stopwatch.Stop();
+                        Console.WriteLine("\nTiempo transcurrido : " + stopwatch.ElapsedMilliseconds + " ms");
+                        continues = false;
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
 
-                   return stringBuilder.ToString();
-               }
-           }
-           
-        } 
+// Da el numero de lineas que debe leer cada particion para que todas lean lo mismo
+// @param totalLineNum numero de lineas totales en el archivo a leer
+// @param totalPartitionNum numero de particiones que vamos a realizar del archivo
+    static int NumberLineDivision(int totalLineNum, int totalPartitionNum)
+    {
+        return totalLineNum / totalPartitionNum;
+    }
+
+// Da el numero en el que debe empezar a leer cada particion
+// @param actualPartitionNum numero de la particion actual (si tenemos 8 particiones, contamos de 0 a 7)
+    static int LineDivisor(int numberLineDivision, int actualPartitionNum)
+    {
+        return numberLineDivision * actualPartitionNum;
+    }
+
+// Encriptador de contraseñas
+    static string CalculateSha256(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            // Convertir cadena en bytes
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            // Calcular el hash
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+            // Convertir los bytes del hash a una cadena hexadecimal
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte b in hashBytes)
+            {
+                stringBuilder.Append(b.ToString("x2"));
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
-
